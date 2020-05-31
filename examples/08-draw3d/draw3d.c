111b34bdd017a505611b62d7e34e0d73eb3ffac8
septag <septag@pm.me>
2020-05-31 13:34:11 +0200

    initiated model import test code

--- examples/08-draw3d/draw3d.c
+++ examples/08-draw3d/draw3d.c
@@ -1,3 +1,4 @@
+#include "sx/io.h"
 #include "sx/math.h"
 #include "sx/os.h"
 #include "sx/string.h"
@@ -8,6 +9,9 @@
 #include "rizz/rizz.h"
 
 #include "../common.h"
+
+#define CGLTF_IMPLEMENTATION
+#include "../3rdparty/cgltf/cgltf.h"
 
 RIZZ_STATE static rizz_api_core* the_core;
 RIZZ_STATE static rizz_api_gfx* the_gfx;
@@ -25,6 +29,189 @@ typedef struct {
 } draw3d_state;
 
 RIZZ_STATE static draw3d_state g_draw3d;
+
+typedef struct rizz_material_texture {
+    rizz_asset tex_asset;
+    int array_index;
+} rizz_material_texture;
+
+typedef struct rizz_material_metallic_roughness {
+    rizz_material_texture base_color_tex;
+    rizz_material_texture metallic_roughness_tex;
+    
+    sx_vec4 base_color_factor;
+    float metallic_factor;
+    float roughness_factor;
+} rizz_material_metallic_roughness;
+
+typedef struct rizz_material_specular_glossiness {
+    rizz_material_texture diffuse_tex;
+    rizz_material_texture specular_glossiness_tex;
+
+    sx_vec4 diffuse_factor;
+    sx_vec3 specular_factor;
+    float glossiness_factor;
+} rizz_material_specular_glossiness;
+
+typedef struct rizz_meterial_clearcoat {
+    rizz_asset clearcoat_tex;
+    rizz_asset clearcoat_roughness_tex;
+    rizz_asset clearcoat_normal_tex;
+
+    float clearcoat_factor;
+    float clearcoat_roughness_factor;
+} rizz_material_clearcoat;
+
+typedef enum rizz_material_alpha_mode {
+    RIZZ_MATERIAL_ALPHAMODE_OPAQUE = 0,
+    RIZZ_MATERIAL_ALPHAMODE_MASK,
+    RIZZ_MATERIAL_ALPHAMODE_BLEND
+} rizz_material_alpha_mode;
+
+typedef struct rizz_material_data {
+    char name[32];
+    bool has_metal_roughness;
+    bool has_specular_glossiness;
+    bool has_clearcoat;
+    bool _reserved1;
+    rizz_material_metallic_roughness pbr_metallic_roughness;
+    rizz_material_specular_glossiness pbr_speuclar_glossiness;
+    rizz_material_clearcoat clearcoat;
+    rizz_material_texture normal_tex;
+    rizz_material_texture occlusion_tex;
+    rizz_material_texture emissive_tex;
+    sx_vec4 emissive_factor;
+    rizz_material_alpha_mode alpha_mode;
+    float alpha_cutoff;
+    bool double_sided;
+    bool unlit;
+} rizz_material_data;
+
+typedef struct { uint32_t id; } rizz_material;
+
+// mandate:
+//  - instance-able
+//  - optionally store gpu buffers for immediate rendering
+//  - joinable (texture-arrays + )
+//  - editable, including instances (dynamic/stream buffers)
+//  - joints and skeleton
+//  - skinning
+//  - wireframe (bc coords)
+//  - compression (quantize)
+//  - multi-stream (base/skin/tangent/extra)
+//      - gbuffer: pos+normal+color+skin+tangent+etc..
+//      - gbuffer (def-light): position+normal+tangent
+//      - shadows: position (+skin)
+//      - light: texcoord + normal
+//  - instancing optimization (some kind of hash?)
+//  - we can pass a vertex layout and model data will be matched by that ! 
+//      - debugging: we need to see all instances of a mesh in the scene
+typedef struct rizz_model_submesh {
+    int start_index;
+    int num_indices;
+    rizz_material mtl;
+} rizz_model_submesh;
+
+typedef enum rizz_model_vertex_attribute {
+    RIZZ_MODEL_VERTEXATTRIBUTE_POSITION = 0,
+    RIZZ_MODEL_VERTEXATTRIBUTE_NORMAL,
+    RIZZ_MODEL_VERTEXATTRIBUTE_TANGENT,
+    RIZZ_MODEL_VERTEXATTRIBUTE_JOINTS,
+    RIZZ_MODEL_VERTEXATTRIBUTE_WEIGHTS,
+    RIZZ_MODEL_VERTEXATTRIBUTE_COLOR0,
+    RIZZ_MODEL_VERTEXATTRIBUTE_COLOR1,
+    RIZZ_MODEL_VERTEXATTRIBUTE_COLOR2,
+    RIZZ_MODEL_VERTEXATTRIBUTE_COLOR3,
+    RIZZ_MODEL_VERTEXATTRIBUTE_TEXCOORD0,
+    RIZZ_MODEL_VERTEXATTRIBUTE_TEXCOORD1,
+    RIZZ_MODEL_VERTEXATTRIBUTE_TEXCOORD2,
+    RIZZ_MODEL_VERTEXATTRIBUTE_TEXCOORD3,
+    _RIZZ_MODEL_VERTEXATTRIBUTE_COUNT
+} rizz_model_vertex_attribute;
+
+// mesh contains the main geometry for each node of the model
+// it includes multiple "submeshes". a submesh is part of the geometry with differnt material
+// so, a mesh can contain multiple materials within itself
+// vertex buffers are another topic that needs to be discussed here:
+//      when loading a model, you should define the whole vertex-layout that your render pipeline uses
+//      so for example, a renderer may need to layout it's vertex data like this:
+//          - buffer #1: position
+//          - buffer #2: normal/tangent
+//          - buffer #3: texcoord
+//          - buffer #4: joints/weights
+//      4 buffers will be reserved (num_vbuffs=4) for every model loaded with this pipeline setup
+//        - if the source model doesn't have joints AND weights. then the buffer #4 slot for the model will be NULL
+//        - if the source model does have normal but does not have tangents, then buffer #2 will be created and tangents will be undefined
+//      When rendering, you can customize which set of buffers you'll need based on the shader's input layout
+//        - shadow map shader: can only fetch the buffer #1 (position)
+//      The catch is when you setup your pipeline, all shaders should comply for one or more vertex-buffer formats
+//      So in our example, every shader must take one of the 4 buffer formats or multiple of them
+// vertex_attrs is all vertex attributes of the source model and is not related for vertex buffer formats
+// gpu_t struct is created only for models without STREAM buffer flag
+typedef struct rizz_model_mesh {
+    int num_submeshes;
+    int num_vertices;
+    int num_indices;
+    int num_vertex_attrs;
+    int num_vbuffs;
+    sg_index_type index_type;
+    rizz_model_vertex_attribute* vertex_attrs;
+    rizz_model_submesh* submeshes;
+
+    struct cpu_t {
+        void** vbuffs;      // arbitary struct for each vbuff (count=num_vbuffs)
+        void*  ibuff;       // data type is based on index_type
+    } cpu;
+
+    struct gpu_t {
+        sg_buffer* vbuffs;
+        sg_buffer  ibuff;
+    } gpu;
+} rizz_model_mesh;
+
+typedef struct rizz_model_node {
+    char name[32];
+    int mesh_id;        // =-1 if it's not renderable
+    int parent_id;      // index to rizz_model.nodes
+    int num_childs;     
+    sx_tx3d local_tx;
+    sx_aabb bounds;
+    int* children;      // indices to rizz_model.nodes
+} rizz_model_node;
+
+// rizz_model will be stored inside rizz_asset handle 
+typedef struct rizz_model {
+    int num_meshes;
+    int num_nodes;
+
+    sx_tx3d root_tx;
+
+    rizz_model_node* nodes;
+    rizz_model_mesh* meshes;
+} rizz_model;
+
+typedef struct { uint32_t id; } rizz_model_instance;
+
+static bool load_model(const char* filepath)
+{
+    sx_mem_block* mem = the_vfs->read(filepath, 0, the_core->alloc(RIZZ_MEMID_GAME));
+    if (!mem) {
+        return false;
+    }
+
+    cgltf_options options = {0};
+    cgltf_data* data = NULL;
+    cgltf_result result = cgltf_parse(&options, mem->data, mem->size, &data);
+    sx_mem_destroy_block(mem);
+    if (result != cgltf_result_success) {
+        return false;
+    }
+
+
+    
+    cgltf_free(data);
+    return true;
+}
 
 static bool init()
 {
